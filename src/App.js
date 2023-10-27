0bb16851d3aa18f1f2ca9d0a9d19ea10b5bc20d1
hsnadeeb <hsnadeeb007@gmail.com>
2023-10-28 02:29:22 +0530

    handeled errors

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [retryTimeout, setRetryTimeout] = useState(null);
 
-  function fetchMoviesHandler() {
-    setIsLoading(true); 
+  useEffect(() => {
+    if (errorMessage) {
+      const timeout = setTimeout(fetchMoviesHandler, 5000);
+      setRetryTimeout(timeout);
+    } else {
+      if (retryTimeout) {
+        clearTimeout(retryTimeout);
+        setRetryTimeout(null);
+      }
+    }
+  }, [errorMessage]);
 
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => response.json())
-      .then((data) => {
-        const transformMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformMovies);
-        setIsLoading(false); 
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setErrorMessage(null);
+
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error("Something went wrong ... Retrying");
+      }
+      const data = await response.json();
+
+      const transformMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
       });
+
+      setMovies(transformMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setErrorMessage(error.message);
+      setIsLoading(false);
+    }
+  }
+
+  function cancelRetryHandler() {
+    if (retryTimeout) {
+      clearTimeout(retryTimeout);
+      setRetryTimeout(null);
+      setErrorMessage(null); 
+    }
   }
 
   return (
@@ -31,11 +62,15 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {isLoading ? (
-          <p>Loading...:-)</p>
-        ) : (
-          <MoviesList movies={movies} />
+        {isLoading && <p>Loading...:-)</p>}
+        {!isLoading && errorMessage && (
+          <p>
+            {errorMessage}
+            <button onClick={cancelRetryHandler}>Cancel</button>
+          </p>
         )}
+        {!isLoading && !errorMessage && movies.length === 0 && <p>No Movies</p>}
+        {!isLoading && !errorMessage && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
